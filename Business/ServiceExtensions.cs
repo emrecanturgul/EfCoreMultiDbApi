983abae89411d659a0b1769e371690fd8ff7fe72
emrecan <turgul2606@gmail.com>
2025-03-01 15:47:04 +0300

    .

---
+++ Business/ServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Business
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddBusinessServices(this IServiceCollection services)
+        {
+            
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());  // Register handlers from the current assembly
+
+            
+
+            return services;
+        }
+    }
+}
--- SqlConnection/Data/Extensions/ServiceExtensions.cs
+++ SqlConnection/Data/Extensions/ServiceExtensions.cs
@@ -2,7 +2,6 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using SqlWebApi.Business.ValidationRules;
 using SqlWebApi.Data.Repositories;
 using SqlWebApi.Models;
 namespace SqlWebApi.Data.Extensions { 
@@ -17,19 +16,7 @@ namespace SqlWebApi.Data.Extensions {
             
             return services;
         }
-        public static IServiceCollection AddCustomServices(this IServiceCollection services)
-        {
-            // FluentValidation için eklentileri IServiceCollection üzerinden yapýlandýrýyoruz.
-            services.AddFluentValidationAutoValidation();
-            services.AddFluentValidationClientsideAdapters();
-
-            // GameValidator'ý ve diðer validatörleri bulmasý için FluentValidation yapýlandýrmasý
-            services.AddValidatorsFromAssemblyContaining<GameValidator>();
-            services.AddMediatR();
-            services.AddControllers(); // AddControllers çaðrýsý en son olabilir.
-            
-            return services;
-        }
+         
 
 
 
